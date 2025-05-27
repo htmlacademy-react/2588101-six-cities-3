0dbb39bb711b7d1e87367c16f305274f763fdf00
alisameta <alisameta09@gmail.com>
2025-05-27 13:00:43 +0300

    4.1 Добавит моки и типы для них

---
+++ src/mocks/offers.ts
@@ -0,0 +1,97 @@
+import {OfferMock} from '../types/offer';
+
+export const offers: OfferMock[] = [
+  {
+    'id': '439c940b-4539-4384-92ee-591ffd03f170',
+    'title': 'Beautiful & luxurious apartment at great location',
+    'type': 'apartment',
+    'price': 100,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/20.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.868610000000004,
+      'longitude': 2.342499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4.4
+  },
+  {
+    'id': '9aa120f5-584c-4bcc-85c3-4e23b7c426d6',
+    'title': 'The Joshua Tree House',
+    'type': 'apartment',
+    'price': 484,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/20.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.858610000000006,
+      'longitude': 2.330499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 2.9
+  },
+  {
+    'id': 'aca7a761-30e9-4445-bacd-a041e28513c0',
+    'title': 'Perfectly located Castro',
+    'type': 'house',
+    'price': 997,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/12.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.834610000000005,
+      'longitude': 2.335499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4.4
+  },
+  {
+    'id': 'de3c3851-dfbd-4f51-b6e8-d6ed2673f535',
+    'title': 'House in countryside',
+    'type': 'hotel',
+    'price': 295,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/11.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.85761,
+      'longitude': 2.358499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 4.2
+  },
+];
+
