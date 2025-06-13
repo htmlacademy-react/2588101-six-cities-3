a956a4699f3adf022dc80d9d15b13d1537ff065c
alisameta <alisameta09@gmail.com>
2025-06-13 14:56:52 +0300

    6.2. Подключит redux к проекту

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {Setting} from './const';
 import {offers} from './mocks/offers';
+import {store} from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,9 +12,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      placeCardCount = {Setting.PlaceCardCount}
-      offers = {offers}
-    />
+    <Provider store = {store}>
+      <App
+        placeCardCount = {Setting.PlaceCardCount}
+        offers = {offers}
+      />
+    </Provider>
   </React.StrictMode>
 );
