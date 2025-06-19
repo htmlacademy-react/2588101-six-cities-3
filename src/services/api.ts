5befe8ea8208bc2f8425e2c699b6a72887a955d7
alisameta <alisameta09@gmail.com>
2025-06-19 17:07:58 +0300

    7.11 Добавит обработку ошибки взаимодействия с сервером

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,21 @@
-import axios, {AxiosInstance, InternalAxiosRequestConfig} from 'axios';
+import axios, {AxiosInstance, InternalAxiosRequestConfig, AxiosResponse, AxiosError} from 'axios';
 import {getToken} from './token';
+import {processErrorHandle} from './process-error-handle';
+import {StatusCodes} from 'http-status-codes';
+
+type DetailMessageType = {
+ type: string;
+ message: string;
+};
 
 const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
@@ -20,6 +33,20 @@ export const createAPI = (): AxiosInstance => {
 
       return config;
     },
+  );
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<DetailMessageType>) => {
+
+      if (error.response && shouldDisplayError(error.response)) {
+        const detailMessage = (error.response.data);
+
+        processErrorHandle(detailMessage.message);
+      }
+
+      throw error;
+    }
   );
 
   return api;
