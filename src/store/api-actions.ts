a3c061fb467721c2c3158d06ee1657c9e9735fa9
alisameta <alisameta09@gmail.com>
2025-06-19 15:46:18 +0300

    7.8 Добавит асинхронное действие для очистки поля error

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -2,8 +2,19 @@ import {AxiosInstance} from 'axios';
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AppDispatch, State} from '../types/state.js';
 import {Offer} from '../types/offer';
-import {initOffers} from './action';
-import {APIRoute} from '../const';
+import {initOffers, setError} from './action';
+import {APIRoute, TIMEOUT_SHOW_ERROR} from '../const';
+import {store} from './';
+
+export const clearErrorAction = createAsyncThunk(
+  'clearError',
+  () => {
+    setTimeout(
+      () => store.dispatch(setError(null)),
+      TIMEOUT_SHOW_ERROR,
+    );
+  },
+);
 
 export const fetchOffersAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
