b1605bc6942c7d6672437f79cca3be8883e1e340
alisameta <alisameta09@gmail.com>
2025-06-25 15:52:05 +0300

    7.3.1 Удалит передачу данных в Private Route через пропсы

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -33,9 +33,7 @@ function App(): JSX.Element {
         <Route
           path={AppRoute.Favorites}
           element={
-            <PrivateRoute
-              authorizationStatus={authorizationStatus}
-            >
+            <PrivateRoute>
               <FavoritesPage />
             </PrivateRoute>
           }
