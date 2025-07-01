c01cf63c45d4d1f4ca87c5e399f7eb1d7ab73c4e
alisameta <alisameta09@gmail.com>
2025-07-01 20:45:28 +0300

    7.3.14 Выносит описание оффера в отдельный компонент

---
+++ src/components/offer-container/offer-container.tsx
@@ -0,0 +1,95 @@
+import classNames from 'classnames';
+import {FullOffer} from '../../types/offer';
+import {capitalizeFirst} from '../../utils';
+
+const STARS_STYLE_COEFF = 20;
+
+type OfferContainerProps = {
+  offer: FullOffer;
+}
+
+function OfferContainer({ offer}: OfferContainerProps): JSX.Element {
+  const {bedrooms, description, goods, host, isPremium, maxAdults, price, rating, title, type} = offer;
+  const {name, avatarUrl, isPro} = host;
+
+  return (
+    <>
+      {isPremium && (
+        <div className="offer__mark">
+          <span>Premium</span>
+        </div>
+      )}
+
+      <div className="offer__name-wrapper">
+        <h1 className="offer__name">
+          {title}
+        </h1>
+        <button className="offer__bookmark-button button" type="button">
+          <svg className="offer__bookmark-icon" width="31" height="33">
+            <use xlinkHref="#icon-bookmark"></use>
+          </svg>
+          <span className="visually-hidden">To bookmarks</span>
+        </button>
+      </div>
+      <div className="offer__rating rating">
+        <div className="offer__stars rating__stars">
+          <span style={{width: `${Math.round(offer.rating) * STARS_STYLE_COEFF}%`}}></span>
+          <span className="visually-hidden">Rating</span>
+        </div>
+        <span className="offer__rating-value rating__value">{rating}</span>
+      </div>
+      <ul className="offer__features">
+        <li className="offer__feature offer__feature--entire">
+          {capitalizeFirst(type)}
+        </li>
+        <li className="offer__feature offer__feature--bedrooms">
+          {bedrooms > 1 ? `${bedrooms} Bedrooms` : `${bedrooms} Bedroom`}
+        </li>
+        <li className="offer__feature offer__feature--adults">
+          {maxAdults > 1 ? `Max ${maxAdults} adults` : `Max ${maxAdults} adult`}
+        </li>
+      </ul>
+      <div className="offer__price">
+        <b className="offer__price-value">&euro;{price}</b>
+        <span className="offer__price-text">&nbsp;night</span>
+      </div>
+      <div className="offer__inside">
+        <h2 className="offer__inside-title">What&apos;s inside</h2>
+        <ul className="offer__inside-list">
+          {goods.map((item) => (
+            <li className="offer__inside-item" key={item}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="offer__host">
+        <h2 className="offer__host-title">Meet the host</h2>
+        <div className="offer__host-user user">
+          <div className={classNames(
+            'offer__avatar-wrapper',
+            'user__avatar-wrapper',
+            {'offer__avatar-wrapper--pro': isPro})}
+          >
+            <img className="offer__avatar user__avatar" src={avatarUrl} width="74" height="74" alt="Host avatar"/>
+          </div>
+          <span className="offer__user-name">
+            {name}
+          </span>
+          {isPro && (
+            <span className="offer__user-status">
+              Pro
+            </span>
+          )}
+        </div>
+        <div className="offer__description">
+          <p className="offer__text">
+            {description}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default OfferContainer;
--- src/pages/offer-page/offer-page.tsx
+++ src/pages/offer-page/offer-page.tsx
@@ -3,6 +3,7 @@ import NearPlacesList from '../../components/near-places-list/near-places-list';
 import ReviewsList from '../../components/reviews-list/reviews-list';
 import CitiesMap from '../../components/cities-map/cities-map';
 import OfferGallery from '../../components/offer-gallery/offer-gallery';
+import OfferContainer from '../../components/offer-container/offer-container';
 import {useAppSelector, useActionCreators} from '../../hooks/types';
 import {useParams} from 'react-router-dom';
 import {useEffect} from 'react';
@@ -49,100 +50,7 @@ function OfferPage(): JSX.Element {
           <OfferGallery images={fullOffer.images} />
           <div className="offer__container container">
             <div className="offer__wrapper">
-              {fullOffer.isPremium &&
-              <div className="offer__mark">
-                <span>Premium</span>
-              </div>}
-              <div className="offer__name-wrapper">
-                <h1 className="offer__name">
-                  {fullOffer.title}
-                </h1>
-                <button className="offer__bookmark-button button" type="button">
-                  <svg className="offer__bookmark-icon" width="31" height="33">
-                    <use xlinkHref="#icon-bookmark"/>
-                  </svg>
-                  <span className="visually-hidden">To bookmarks</span>
-                </button>
-              </div>
-              <div className="offer__rating rating">
-                <div className="offer__stars rating__stars">
-                  <span style={{width: `${Math.round(fullOffer.rating) * 20}%`}}/>
-                  <span className="visually-hidden">Rating</span>
-                </div>
-                <span className="offer__rating-value rating__value">{fullOffer.rating}</span>
-              </div>
-              <ul className="offer__features">
-                <li className="offer__feature offer__feature--entire">
-                  Apartment
-                </li>
-                <li className="offer__feature offer__feature--bedrooms">
-                  3 Bedrooms
-                </li>
-                <li className="offer__feature offer__feature--adults">
-                  Max 4 adults
-                </li>
-              </ul>
-              <div className="offer__price">
-                <b className="offer__price-value">&euro;{fullOffer.price}</b>
-                <span className="offer__price-text">&nbsp;night</span>
-              </div>
-              <div className="offer__inside">
-                <h2 className="offer__inside-title">What&apos;s inside</h2>
-                <ul className="offer__inside-list">
-                  <li className="offer__inside-item">
-                    Wi-Fi
-                  </li>
-                  <li className="offer__inside-item">
-                    Washing machine
-                  </li>
-                  <li className="offer__inside-item">
-                    Towels
-                  </li>
-                  <li className="offer__inside-item">
-                    Heating
-                  </li>
-                  <li className="offer__inside-item">
-                    Coffee machine
-                  </li>
-                  <li className="offer__inside-item">
-                    Baby seat
-                  </li>
-                  <li className="offer__inside-item">
-                    Kitchen
-                  </li>
-                  <li className="offer__inside-item">
-                    Dishwasher
-                  </li>
-                  <li className="offer__inside-item">
-                    Cabel TV
-                  </li>
-                  <li className="offer__inside-item">
-                    Fridge
-                  </li>
-                </ul>
-              </div>
-              <div className="offer__host">
-                <h2 className="offer__host-title">Meet the host</h2>
-                <div className="offer__host-user user">
-                  <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="offer__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar"/>
-                  </div>
-                  <span className="offer__user-name">
-                    Angelina
-                  </span>
-                  <span className="offer__user-status">
-                    Pro
-                  </span>
-                </div>
-                <div className="offer__description">
-                  <p className="offer__text">
-                    A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                  </p>
-                  <p className="offer__text">
-                    An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.
-                  </p>
-                </div>
-              </div>
+              <OfferContainer offer={fullOffer} />
               <section className="offer__reviews reviews">
                 {reviews.length > 0 &&
                 <>
