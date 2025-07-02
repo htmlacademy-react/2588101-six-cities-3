1d73966774d215996e806d722bb910dbad163b8b
alisameta <alisameta09@gmail.com>
2025-07-02 17:34:24 +0300

    8.2.2 Добавит рендер main-empty на главной странице в отсутствии предложений

--- src/components/main-empty/main-empty.tsx
+++ src/components/main-empty/main-empty.tsx
@@ -1,38 +1,18 @@
-import CitiesList from '../../components/cities-list/cities-list';
-import {useAppSelector} from '../../hooks/types';
-import {City} from '../../types/offer';
-import {getCity} from '../../store/app-process/app-process.selectors';
-
 type MainEmptyProps = {
-citiesList: City[];
-};
+  activeCityName: string;
+}
 
-function MainEmpty({citiesList}: MainEmptyProps): JSX.Element {
-  const activeCity = useAppSelector(getCity);
+function MainEmpty({activeCityName}: MainEmptyProps): JSX.Element {
 
   return (
-    <div className="page page--gray page--main">
-      <main className="page__main page__main--index page__main--index-empty">
-        <h1 className="visually-hidden">Cities</h1>
-        <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <CitiesList citiesList={citiesList} />
-            </ul>
-          </section>
+    <div className="cities__places-container cities__places-container--empty container">
+      <section className="cities__no-places">
+        <div className="cities__status-wrapper tabs__content">
+          <b className="cities__status">No places to stay available</b>
+          <p className="cities__status-description">We could not find any property available at the moment in {activeCityName}</p>
         </div>
-        <div className="cities">
-          <div className="cities__places-container cities__places-container--empty container">
-            <section className="cities__no-places">
-              <div className="cities__status-wrapper tabs__content">
-                <b className="cities__status">No places to stay available</b>
-                <p className="cities__status-description">We could not find any property available at the moment in {activeCity.name}</p>
-              </div>
-            </section>
-            <div className="cities__right-section"></div>
-          </div>
-        </div>
-      </main>
+      </section>
+      <div className="cities__right-section"></div>
     </div>
   );
 }
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -3,6 +3,7 @@ import CitiesMap from '../../components/cities-map/cities-map';
 import Header from '../../components/header/header';
 import CitiesList from '../../components/cities-list/cities-list';
 import PlacesSorting from '../../components/places-sorting/places-sorting';
+import MainEmpty from '../../components/main-empty/main-empty';
 import {useState} from 'react';
 import {SortOption, CITIES} from '../../const';
 import {useAppSelector} from '../../hooks/types';
@@ -38,7 +39,8 @@ function MainPage(): JSX.Element {
       <header className="header">
         <Header />
       </header>
-      <main className="page__main page__main--index">
+
+      <main className={`page__main page__main--index ${offers.length === 0 && 'page__main--index-empty'}`}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -46,31 +48,34 @@ function MainPage(): JSX.Element {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{activeCityOffers.length} place{(activeCityOffers.length > 1 || activeCityOffers.length === 0) && 's'} to stay in {activeCity.name}</b>
-              {activeCityOffers.length > 0 && <PlacesSorting current={activeSort} setter={setActiveSort} />}
-              <div className="cities__places-list places__list tabs__content">
-                <OffersList
-                  onHandleChangeActiveId={handleChangeActiveId}
-                  offers={sortedOffers}
-                />
-              </div>
-            </section>
-            <div className="cities__right-section">
-              <section
-                style={{width: '100%'}}
-                className={`${offers.length < 0 && 'cities__map'} map`}
-              >
-                <CitiesMap
-                  city={activeCity}
-                  offers={activeCityOffers}
-                  activeOfferId={activeOfferId}
-                />
+          {offers.length === 0 ?
+            <MainEmpty activeCityName={activeCity.name} />
+            :
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{activeCityOffers.length} place{(activeCityOffers.length > 1 || activeCityOffers.length === 0) && 's'} to stay in {activeCity.name}</b>
+                {activeCityOffers.length > 0 && <PlacesSorting current={activeSort} setter={setActiveSort} />}
+                <div className="cities__places-list places__list tabs__content">
+                  <OffersList
+                    onHandleChangeActiveId={handleChangeActiveId}
+                    offers={sortedOffers}
+                  />
+                </div>
               </section>
-            </div>
-          </div>
+              <div className="cities__right-section">
+                <section
+                  style={{width: '100%'}}
+                  className={`${offers.length < 0 && 'cities__map'} map`}
+                >
+                  <CitiesMap
+                    city={activeCity}
+                    offers={activeCityOffers}
+                    activeOfferId={activeOfferId}
+                  />
+                </section>
+              </div>
+            </div>}
         </div>
       </main>
     </div>
