1a27dac7bb87cb0c0b704834fe461389a44c37f5
alisameta <alisameta09@gmail.com>
2025-07-06 15:30:45 +0300

    9.2.6 Реализует доп задание: активный кнопка со случайным городом на странице логин

--- src/utils.ts
+++ src/utils.ts
@@ -11,3 +11,10 @@ export const capitalizeFirst = (string: string) => {
 export function sortReviewsByDate(a: Review, b: Review) {
   return new Date(b.date).getTime() - new Date(a.date).getTime();
 }
+
+export const getRandomInteger = (min: number, max: number) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
